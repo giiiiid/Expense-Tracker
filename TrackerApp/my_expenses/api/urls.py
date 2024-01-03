8a56042b6e0286a26e215936ed3d82eed681ede7
Giiid <gideonfummey1@gmail.com>
2024-01-03 13:29:55 +0000

    files config

--- TrackerApp/my_expenses/api/urls.py
+++ TrackerApp/my_expenses/api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("create-list-expense", views.expense_list_create, name="create_list_expense"),
+    # path("create-list-expense", views.expense_list_create, name="create_list_expense"),
     path("clexpense", views.ExpenseList.as_view(), name="clexpense")
 ]
